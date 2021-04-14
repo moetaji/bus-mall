21825c155dfc1799940c0f8e279eb8c7aa6038b5
moetaji <mohmmad.altaji@gmail.com>
2021-04-14 22:58:01 +0300

    add localstorage

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # bus-mall
+
 this bus-mall project made by mohammad altaji
 
 - How did this go, overall?
@@ -11,4 +12,9 @@ i don't have any
 
 - How long did it take you to complete this assignment? And, before you started, how long did you think it would take you to complete this assignment?
 
-it took me about 4 hour to finsh 
+i didn't finsh all the lab requirement 
+
+
+
+updat for lab13
+
--- js/app.js
+++ js/app.js
@@ -80,20 +80,24 @@ console.log(Busproudect.allProuduct);
 function randomProduct() {
     return Math.floor(Math.random() * Busproudect.allProuduct.length);
 }
-// randomProduct();
 
+
+let holderProduct = [];
 //render funcion
 function render() {
-    imageIndexLeft = randomProduct();
+
     imageIndexRight = randomProduct();
     imageIndexCenter = randomProduct();
+    imageIndexLeft = randomProduct();
 
-
-    while (imageIndexLeft === imageIndexRight || imageIndexRight === imageIndexCenter) {
+    while (imageIndexLeft === imageIndexRight || imageIndexLeft === imageIndexCenter || imageIndexCenter === imageIndexRight || holderProduct.includes(imageIndexLeft) || holderProduct.includes(imageIndexCenter) || holderProduct.includes(imageIndexRight)) {
         imageIndexRight = randomProduct();
         imageIndexCenter = randomProduct();
-
+        imageIndexLeft = randomProduct();
     }
+
+    holderProduct = [imageIndexLeft, imageIndexRight, imageIndexCenter];
+
     leftImagElment.src = Busproudect.allProuduct[imageIndexLeft].source;
     Busproudect.allProuduct[imageIndexLeft].shown++;
     centerImageElment.src = Busproudect.allProuduct[imageIndexCenter].source;
@@ -103,6 +107,7 @@ function render() {
     Busproudect.allProuduct[imageIndexRight].shown++;
 
 }
+
 render();
 
 //eventlistner
@@ -203,3 +208,23 @@ function chart() {
     });
 
 }
+
+function storedata() {
+    let arrayString = JSON.stringify(Busproudect.allProuduct);
+
+    // console.log(arrayString);
+    localStorage.setItem('shown', arrayString);
+
+}
+
+function getData() {
+    let data = localStorage.getItem('shown');
+    // console.log(data);
+    let shownData = JSON.parse(data);
+    console.log(shownData);
+}
+render();
+
+storedata();
+
+getData();
